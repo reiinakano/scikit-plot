6a8ffa940a582ae1a44624e6683d1deb97aaf907
reiinakano <reii_nakano@yahoo.com>
2017-02-13 00:29:25 +0800

    with mock

--- docs/conf.py
+++ docs/conf.py
@@ -16,10 +16,20 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+from mock import Mock as MagicMock
 
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['scipy', 'numpy', 'pandas', 'matplotlib']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
