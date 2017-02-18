a0254dee8ae80fd4082acbd5cee82aa8f094f4b8
reiinakano <reii_nakano@yahoo.com>
2017-02-19 05:15:02 +0800

    update version to 0.2.0

--- scikitplot/__init__.py
+++ scikitplot/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-__version__ = '0.1'
+__version__ = '0.2.0'
 
 
 from scikitplot.classifiers import classifier_factory
