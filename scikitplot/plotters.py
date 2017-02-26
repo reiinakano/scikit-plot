9b78158b8bb98480323942a461bb4d42b45eb9c9
reiinakano <reiichiro.s.nakano@gmail.com>
2017-02-27 00:44:48 +0800

    add figsize, title_fontsize, and text_fontsize to plot_learning_curve

--- scikitplot/plotters.py
+++ scikitplot/plotters.py
@@ -485,7 +485,7 @@ def plot_feature_importances(clf, title='Feature Importance', feature_names=None
 
 
 def plot_learning_curve(clf, X, y, title='Learning Curve', cv=None, train_sizes=None, n_jobs=1,
-                        ax=None):
+                        ax=None, figsize=None, title_fontsize="large", text_fontsize="medium"):
     """Generates a plot of the train and test learning curves for a given classifier.
 
     Args:
@@ -522,6 +522,15 @@ def plot_learning_curve(clf, X, y, title='Learning Curve', cv=None, train_sizes=
         ax (:class:`matplotlib.axes.Axes`, optional): The axes upon which to plot
             the learning curve. If None, the plot is drawn on a new set of axes.
 
+        figsize (2-tuple, optional): Tuple denoting figure size of the plot e.g. (6, 6).
+            Defaults to ``None``.
+
+        title_fontsize (string or int, optional): Matplotlib-style fontsizes.
+            Use e.g. "small", "medium", "large" or integer-values. Defaults to "large".
+
+        text_fontsize (string or int, optional): Matplotlib-style fontsizes.
+            Use e.g. "small", "medium", "large" or integer-values. Defaults to "medium".
+
     Returns:
         ax (:class:`matplotlib.axes.Axes`): The axes on which the plot was drawn.
 
@@ -537,14 +546,14 @@ def plot_learning_curve(clf, X, y, title='Learning Curve', cv=None, train_sizes=
            :alt: Learning Curve
     """
     if ax is None:
-        fig, ax = plt.subplots(1, 1)
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
 
     if train_sizes is None:
         train_sizes = np.linspace(.1, 1.0, 5)
 
-    ax.set_title(title)
-    ax.set_xlabel("Training examples")
-    ax.set_ylabel("Score")
+    ax.set_title(title, fontsize=title_fontsize)
+    ax.set_xlabel("Training examples", fontsize=text_fontsize)
+    ax.set_ylabel("Score", fontsize=text_fontsize)
     train_sizes, train_scores, test_scores = learning_curve(
         clf, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -560,7 +569,8 @@ def plot_learning_curve(clf, X, y, title='Learning Curve', cv=None, train_sizes=
             label="Training score")
     ax.plot(train_sizes, test_scores_mean, 'o-', color="g",
             label="Cross-validation score")
-    ax.legend(loc="best")
+    ax.tick_params(labelsize=text_fontsize)
+    ax.legend(loc="best", fontsize=text_fontsize)
 
     return ax
 
