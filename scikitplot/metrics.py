793983ec2d2327389c16d50b12a5aae337d2d285
reiinakano <reiichiro.s.nakano@gmail.com>
2017-09-10 02:53:53 +0800

    add plot_roc_curve  in metrics, modify example, add test

--- scikitplot/metrics.py
+++ scikitplot/metrics.py
@@ -170,3 +170,150 @@ def plot_confusion_matrix(y_true, y_pred, labels=None, true_labels=None,
     ax.grid('off')
 
     return ax
+
+
+def plot_roc_curve(y_true, y_probas, title='ROC Curves',
+                   curves=('micro', 'macro', 'each_class'),
+                   ax=None, figsize=None, cmap='spectral',
+                   title_fontsize="large", text_fontsize="medium"):
+    """Generates the ROC curves from labels and predicted scores/probabilities
+
+    Args:
+        y_true (array-like, shape (n_samples)):
+            Ground truth (correct) target values.
+
+        y_probas (array-like, shape (n_samples, n_classes)):
+            Prediction probabilities for each class returned by a classifier.
+
+        title (string, optional): Title of the generated plot. Defaults to
+            "ROC Curves".
+
+        curves (array-like): A listing of which curves should be plotted on the
+            resulting plot. Defaults to `("micro", "macro", "each_class")`
+            i.e. "micro" for micro-averaged curve, "macro" for macro-averaged
+            curve
+
+        ax (:class:`matplotlib.axes.Axes`, optional): The axes upon which to
+            plot the curve. If None, the plot is drawn on a new set of axes.
+
+        figsize (2-tuple, optional): Tuple denoting figure size of the plot
+            e.g. (6, 6). Defaults to ``None``.
+
+        cmap (string or :class:`matplotlib.colors.Colormap` instance, optional):
+            Colormap used for plotting the projection. View Matplotlib Colormap
+            documentation for available options.
+            https://matplotlib.org/users/colormaps.html
+
+        title_fontsize (string or int, optional): Matplotlib-style fontsizes.
+            Use e.g. "small", "medium", "large" or integer-values. Defaults to
+            "large".
+
+        text_fontsize (string or int, optional): Matplotlib-style fontsizes.
+            Use e.g. "small", "medium", "large" or integer-values. Defaults to
+            "medium".
+
+    Returns:
+        ax (:class:`matplotlib.axes.Axes`): The axes on which the plot was
+            drawn.
+
+    Example:
+        >>> import scikitplot as skplt
+        >>> nb = GaussianNB()
+        >>> nb = nb.fit(X_train, y_train)
+        >>> y_probas = nb.predict_proba(X_test)
+        >>> skplt.metrics.plot_roc_curve(y_test, y_probas)
+        <matplotlib.axes._subplots.AxesSubplot object at 0x7fe967d64490>
+        >>> plt.show()
+
+        .. image:: _static/examples/plot_roc_curve.png
+           :align: center
+           :alt: ROC Curves
+    """
+    y_true = np.array(y_true)
+    y_probas = np.array(y_probas)
+
+    if 'micro' not in curves and 'macro' not in curves and \
+            'each_class' not in curves:
+        raise ValueError('Invalid argument for curves as it '
+                         'only takes "micro", "macro", or "each_class"')
+
+    classes = np.unique(y_true)
+    probas = y_probas
+
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(len(classes)):
+        fpr[i], tpr[i], _ = roc_curve(y_true, probas[:, i],
+                                      pos_label=classes[i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    micro_key = 'micro'
+    i = 0
+    while micro_key in fpr:
+        i += 1
+        micro_key += str(i)
+
+    y_true = label_binarize(y_true, classes=classes)
+    if len(classes) == 2:
+        y_true = np.hstack((1 - y_true, y_true))
+
+    fpr[micro_key], tpr[micro_key], _ = roc_curve(y_true.ravel(),
+                                                  probas.ravel())
+    roc_auc[micro_key] = auc(fpr[micro_key], tpr[micro_key])
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[x] for x in range(len(classes))]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(len(classes)):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= len(classes)
+
+    macro_key = 'macro'
+    i = 0
+    while macro_key in fpr:
+        i += 1
+        macro_key += str(i)
+    fpr[macro_key] = all_fpr
+    tpr[macro_key] = mean_tpr
+    roc_auc[macro_key] = auc(fpr[macro_key], tpr[macro_key])
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    if 'each_class' in curves:
+        for i in range(len(classes)):
+            color = plt.cm.get_cmap(cmap)(float(i) / len(classes))
+            ax.plot(fpr[i], tpr[i], lw=2, color=color,
+                    label='ROC curve of class {0} (area = {1:0.2f})'
+                    ''.format(classes[i], roc_auc[i]))
+
+    if 'micro' in curves:
+        ax.plot(fpr[micro_key], tpr[micro_key],
+                label='micro-average ROC curve '
+                      '(area = {0:0.2f})'.format(roc_auc[micro_key]),
+                color='deeppink', linestyle=':', linewidth=4)
+
+    if 'macro' in curves:
+        ax.plot(fpr[macro_key], tpr[macro_key],
+                label='macro-average ROC curve '
+                      '(area = {0:0.2f})'.format(roc_auc[macro_key]),
+                color='navy', linestyle=':', linewidth=4)
+
+    ax.plot([0, 1], [0, 1], 'k--', lw=2)
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate', fontsize=text_fontsize)
+    ax.set_ylabel('True Positive Rate', fontsize=text_fontsize)
+    ax.tick_params(labelsize=text_fontsize)
+    ax.legend(loc='lower right', fontsize=text_fontsize)
+    return ax
--- scikitplot/tests/test_metrics.py
+++ scikitplot/tests/test_metrics.py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scikitplot.metrics import plot_confusion_matrix
+from scikitplot.metrics import plot_roc_curve
 
 
 def convert_labels_into_string(y_true):
@@ -84,3 +85,61 @@ class TestPlotConfusionMatrix(unittest.TestCase):
 
     def test_array_like(self):
         plot_confusion_matrix([0, 1], [1, 0])
+
+
+class TestPlotROCCurve(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+        self.X, self.y = load_data(return_X_y=True)
+        p = np.random.permutation(len(self.X))
+        self.X, self.y = self.X[p], self.y[p]
+
+    def tearDown(self):
+        plt.close("all")
+
+    def test_string_classes(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        clf.fit(self.X, self.y)
+        probas = clf.predict_proba(self.X)
+        plot_roc_curve(convert_labels_into_string(self.y), probas)
+
+    def test_ax(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        clf.fit(self.X, self.y)
+        probas = clf.predict_proba(self.X)
+        fig, ax = plt.subplots(1, 1)
+        out_ax = plot_roc_curve(self.y, probas)
+        assert ax is not out_ax
+        out_ax = plot_roc_curve(self.y, probas, ax=ax)
+        assert ax is out_ax
+
+    def test_cmap(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        clf.fit(self.X, self.y)
+        probas = clf.predict_proba(self.X)
+        plot_roc_curve(self.y, probas, cmap='spectral')
+        plot_roc_curve(self.y, probas, cmap=plt.cm.spectral)
+
+    def test_curve_diffs(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        clf.fit(self.X, self.y)
+        probas = clf.predict_proba(self.X)
+        ax_macro = plot_roc_curve(self.y, probas, curves='macro')
+        ax_micro = plot_roc_curve(self.y, probas, curves='micro')
+        ax_class = plot_roc_curve(self.y, probas, curves='each_class')
+        self.assertNotEqual(ax_macro, ax_micro, ax_class)
+
+    def test_invalid_curve_arg(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        clf.fit(self.X, self.y)
+        probas = clf.predict_proba(self.X)
+        self.assertRaises(ValueError, plot_roc_curve, self.y, probas,
+                          curves='zzz')
+
+    def test_array_like(self):
+        plot_roc_curve([0, 1], [[0.8, 0.2], [0.2, 0.8]])
