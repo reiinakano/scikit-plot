006078373c2a45a83c0266c0a80f8c72f95629fa
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2017-09-10 17:12:32 +0800

    add plot_learning_curve to estimators module + test + example (#56)

--- scikitplot/tests/test_estimators.py
+++ scikitplot/tests/test_estimators.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scikitplot.estimators import plot_feature_importances
+from scikitplot.estimators import plot_learning_curve
 
 
 def convert_labels_into_string(y_true):
@@ -67,4 +68,52 @@ class TestFeatureImportances(unittest.TestCase):
         out_ax = plot_feature_importances(clf)
         assert ax is not out_ax
         out_ax = plot_feature_importances(clf, ax=ax)
+        assert ax is out_ax
+
+
+class TestPlotLearningCurve(unittest.TestCase):
+
+    def setUp(self):
+        np.random.seed(0)
+        self.X, self.y = load_data(return_X_y=True)
+        p = np.random.permutation(len(self.X))
+        self.X, self.y = self.X[p], self.y[p]
+
+    def tearDown(self):
+        plt.close("all")
+
+    def test_string_classes(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        plot_learning_curve(clf, self.X, convert_labels_into_string(self.y))
+
+    def test_cv(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        plot_learning_curve(clf, self.X, self.y)
+        plot_learning_curve(clf, self.X, self.y, cv=5)
+
+    def test_train_sizes(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        plot_learning_curve(clf, self.X, self.y,
+                            train_sizes=np.linspace(0.1, 1.0, 8))
+
+    def test_n_jobs(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        plot_learning_curve(clf, self.X, self.y, n_jobs=-1)
+
+    def test_random_state_and_shuffle(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        plot_learning_curve(clf, self.X, self.y, random_state=1, shuffle=True)
+
+    def test_ax(self):
+        np.random.seed(0)
+        clf = LogisticRegression()
+        fig, ax = plt.subplots(1, 1)
+        out_ax = plot_learning_curve(clf, self.X, self.y)
+        assert ax is not out_ax
+        out_ax = plot_learning_curve(clf, self.X, self.y, ax=ax)
         assert ax is out_ax
